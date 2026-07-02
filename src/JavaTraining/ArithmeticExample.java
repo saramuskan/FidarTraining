1debae51321e13646cf54167d5c857f8dda9ee67
saramuskan <sara.muskan92@gmail.com>
2026-07-02 23:11:40 +0530

    Java Programs

---
+++ src/JavaTraining/ArithmeticExample.java
@@ -0,0 +1,23 @@
+package JavaTraining;
+
+public class ArithmeticExample {
+
+    public static void main(String[] args) {
+
+        int a = 20;
+        int b = 10;
+        float c = 5.5f;
+        double d = 2.5;
+        char ch = 'A';
+
+        System.out.println("Character: " + ch);
+
+        System.out.println("Addition: " + (a + b));
+        System.out.println("Subtraction: " + (a - b));
+        System.out.println("Multiplication: " + (a * b));
+        System.out.println("Division: " + (a / b));
+        System.out.println("Modulus: " + (a % b));
+
+        System.out.println("Float Addition: " + (c + d));
+    }
+}
