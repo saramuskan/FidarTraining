be7e43e604b3911127356dc52f3cddfe2ea5931a
saramuskan <sara.muskan92@gmail.com>
2026-07-03 14:04:49 +0530

    Java Programs

---
+++ src/JavaTraining/Sum.java
@@ -0,0 +1,15 @@
+package JavaTraining;
+
+public class Sum {
+
+	public static void main(String[] args) {
+		int a = 34;
+		int b = 12;
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a/b);
+		System.out.println(a*b);
+
+	}
+
+}
