d465641615018242fe8f89a218f67a57c517f180
saramuskan <sara.muskan92@gmail.com>
2026-07-03 14:12:45 +0530

    Java Programs

---
+++ src/JavaTraining/SumOfNumbers.java
@@ -0,0 +1,16 @@
+package JavaTraining;
+
+public class SumOfNumbers {
+
+	public static void main(String[] args) {
+		System.out.println("add three numbers");
+		int num1 = 12;
+		int num2 = 23;
+		int num3 = 34;
+		int sum = num1 + num2 + num3;
+		
+		System.out.println("sum of numbers is " + sum );
+
+	}
+
+}
