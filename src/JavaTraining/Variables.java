e8f34bde016bfd12727e6a778e86dac7692b5337
saramuskan <sara.muskan92@gmail.com>
2026-07-03 13:59:50 +0530

    Java Programs

---
+++ src/JavaTraining/Variables.java
@@ -0,0 +1,28 @@
+package JavaTraining;
+
+public class Variables {
+
+	public static void main(String[] args) {
+		double d = 10;           // implicit casting
+		System.out.println(d);
+		
+		long a = 234567890l;
+		System.out.println(a);
+		
+		long b = 23;
+		System.out.println(b);
+		
+		
+//		System.out.println("==size==");
+//		short s = 5;
+//		byte b =5;
+//		
+//		 byte b = 5;
+//		 short s= b;
+//		 System.out.println(b);
+//		 System.out.println(s);
+		 
+
+	}
+
+}
