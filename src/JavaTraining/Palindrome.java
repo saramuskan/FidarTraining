218e7d470688f93c62c1068f4503923b9c77204b
saramuskan <sara.muskan92@gmail.com>
2026-07-06 22:37:59 +0530

    Java Programs

---
+++ src/JavaTraining/Palindrome.java
@@ -0,0 +1,36 @@
+package JavaTraining;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a number:");
+        int num = sc.nextInt();
+
+        int original = num;
+        int reverse = 0;
+
+        while (num > 0) {
+
+            int rem = num % 10;
+
+            reverse = reverse * 10 + rem;
+
+            num = num / 10;
+        }
+
+        if (original == reverse) {
+            System.out.println("Palindrome Number");
+        } else {
+            System.out.println("Not a Palindrome Number");
+        }
+
+        sc.close();
+    }
+
+	}
+
+
