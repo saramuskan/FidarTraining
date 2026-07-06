317e5672789986b23df81ad5af175f2c05232c5a
saramuskan <sara.muskan92@gmail.com>
2026-07-06 15:42:01 +0530

    Java Programs

---
+++ src/JavaTraining/AddNum.java
@@ -0,0 +1,15 @@
+package JavaTraining;
+
+public class AddNum {
+
+	public static void main(String[] args) {
+	
+	int a = 66;
+	int b = 59;
+	int c = 77;
+	int sum = a+ b+ c;
+	System.out.println("sum of 3 numbers is: " +sum);
+
+	}
+
+}
