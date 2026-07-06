1c1f117f1e198dd88b39559dba1f66b3950c2f63
saramuskan <sara.muskan92@gmail.com>
2026-07-06 21:13:23 +0530

    Java Programs

---
+++ src/JavaTraining/PrintUserName.java
@@ -0,0 +1,16 @@
+package JavaTraining;
+
+import java.util.Scanner;
+
+public class PrintUserName {
+
+	public static void main(String[] args) {
+	System.out.println("what's your name");
+	Scanner sc = new Scanner(System.in);
+	String name = sc.next();
+	System.out.println("hello " +name+ " have great day!");
+			
+
+	}
+
+}
